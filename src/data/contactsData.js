02f50b2418dfea5bd99e96cb723f50b73325d594
Sanjay Khatri <www.khatrisanjay@gmail.com>
2024-01-01 15:20:21 +0545

    first commit

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'ishaghimere@gmail.com',
+    phone: '9818916428',
+    address: 'Kapan, Kathmandu, Nepal ',
+
+    sheetAPI: ''
+}
---
+++ src/data/educationData.js
@@ -0,0 +1,23 @@
+export const educationData = [
+    {
+        id: 1,
+        institution: 'Sypres Public School',
+        course: 'Higher Secondary Education',
+        startYear: '2012',
+        endYear: '2015'
+    },
+    {
+        id: 2,
+        institution: 'Reliance International Academy',
+        course: 'Reliance International Academy',
+        startYear: '2018',
+        endYear: '2020'
+    },
+    {
+        id: 3,
+        institution: 'Aadim National College ',
+        course: 'Bachelor In Computer Application',
+        startYear: '2021',
+        endYear: '2022'
+    },
+]
